3e199a62536b9273c3d2e681885bada64d7bfd4c
Tetyana Murzina <tetyanamur@gmail.com>
2022-07-27 20:20:58 -0500

    initial commit

---
+++ Variables_Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Rex";
+            int dogAge = 2;
+            Console.WriteLine($"My dog's name is{dogName}, He is {dogAge} years old");
+        }
+    }
+}
